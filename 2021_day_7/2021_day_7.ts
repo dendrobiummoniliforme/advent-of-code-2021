f51397d2bfda34a8e59936e45e58223e14a893e9
Dendrobium Moniliforme <dendrobiummoniliforme0@gmail.com>
2021-12-07 17:16:14 -0500

    Finish Day 7.

--- 2021_day_7/2021_day_7.ts
+++ 2021_day_7/2021_day_7.ts
@@ -1,7 +1,41 @@
 import { data } from "../lib";
 
- // 1. Pre-process.
-const words: string[] = data(`${__dirname}/test_input.txt`).split(',');
+/**
+ * horizontalAlignment determines the amount of fuel used to minimize aligning crabs. They might be raving.
+ * @param currentMaximum current maximum value of the array given.
+ * @param numbers the list of numbers given.
+ * @param fuel the current fuel used.
+ * @param rave self-explanatory.
+ */
+ const horizontalAlignment = (currentMaximum: number, numbers: number[], fuel: number, rave: boolean = false) => {
+    let distance: number[] = numbers.map(element => Math.abs(element - currentMaximum));   
+    let previousFuel: number = fuel;
+
+    if (rave === true) {
+        distance = distance.map(element => {
+            let temp: number = 0;
+            for (let i = 0; i <= element; i++) {
+                temp = temp + i;
+            }
+            return temp;
+        });
+    }
+
+    fuel = distance.reduce((previous, current) => previous + current);
+    if (fuel <= previousFuel) {
+        currentMaximum--
+        horizontalAlignment(currentMaximum, numbers, fuel, rave);
+    }
+
+    if (fuel > previousFuel) {
+        console.log("horizontal", previousFuel);
+        
+    }
+
+}
+
+// 1. Pre-process.
+const words: string[] = data(`${__dirname}/input.txt`).split(',');
 
 // 2. Convert array to numbers, and sort ascending.
 const numbers: number[] = words.map(
@@ -10,27 +44,27 @@ const numbers: number[] = words.map(
     }
 ).sort((a, b) => a - b);
 
-const globalMaximum: number = numbers[numbers.length-1] - numbers[0];
+// 3. (Part A) Set initial values.
+let globalMaximum: number = numbers[numbers.length-1] - numbers[0];
+let distance: number[] = numbers.map(element => Math.abs(element - globalMaximum));
+let fuel = distance.reduce((previous, current) => previous + current);
 
-const horizontalAlignment = (currentMaximum: number, numbers: number[], fuel: number, previousMaximum: number) => {
-    const distance: number[] = numbers.map(element => Math.abs(element - currentMaximum));    
-    let previousFuel: number = fuel;
+// Part A.
+console.log("\n",globalMaximum, numbers, fuel);
+horizontalAlignment(globalMaximum, numbers, fuel, false);
 
-    fuel = distance.reduce((previous, current) => previous + current);
-    if (fuel <= previousFuel) {
-        currentMaximum--
-        horizontalAlignment(currentMaximum, numbers, fuel, previousMaximum);
+// 3. (Part B) Set initial values.
+globalMaximum = numbers[numbers.length-1] - numbers[0];
+distance = numbers.map(element => Math.abs(element - globalMaximum));
+distance = distance.map(element => {
+    let temp: number = 0;
+    for (let i = 0; i <= element; i++) {
+        temp = temp + i;
     }
+    return temp;
+});
+fuel = distance.reduce((previous, current) => previous + current);
 
-    if (fuel > previousFuel) {
-        console.log("horizontal", previousFuel);
-        
-    }
-    //console.log("horizontal", fuel);
-}
-
-const distance: number[] = numbers.map(element => Math.abs(element - globalMaximum));
-const fuel = distance.reduce((previous, current) => previous + current);
-
-horizontalAlignment(globalMaximum, numbers, fuel, globalMaximum);
-console.log(numbers, globalMaximum);
+// Part B.
+console.log("\n",globalMaximum, numbers, fuel);
+horizontalAlignment(globalMaximum, numbers, fuel, true);
